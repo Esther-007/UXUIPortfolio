ce5278eace1e68d484e41b67e5faf29bda54b381
Esther-007 <68214568+Esther-007@users.noreply.github.com>
2021-01-08 21:36:44 -0800

    adding new page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-const parallax = document.getElementById("parallax");
-
-window.addEventListener("scroll", function () {
-    let offset = window.pageYOffset;
-    parallax.style.backgroundPositionY = offset * 0.7 + "px";
-    // console.log('Offset: ' + offset);
-    // console.log('offset * 0.7' + offset * 0.7);
+console.log( $(".menuIconMobile") );
+$(".menuIconMobile").on("click", function(){
+    // console.log("i'm clicking!")
+    $(".toggleMenu").toggleClass("active");
 });
+
+
+
+// const parallax = document.getElementById("parallax");
+
+// window.addEventListener("scroll", function () {
+//     let offset = window.pageYOffset;
+//     parallax.style.backgroundPositionY = offset * 0.7 + "px";
+//     // console.log('Offset: ' + offset);
+//     // console.log('offset * 0.7' + offset * 0.7);
+// });
+
